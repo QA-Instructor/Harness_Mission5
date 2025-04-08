670ae08e8b1cf92f2d29b8620c719732a9a73ec8
Victoria Lloyd <QAInstructorSky@gmail.com>
2025-04-08 13:42:11 +0100

    Added Hannah

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -16,6 +16,7 @@ public class App
         System.out.println(sayHelloToSomeone("Edwin"));
         System.out.println(sayHelloToSomeone("Fowsiya"));
         System.out.println(sayHelloToSomeone("Guiseppe"));
+        System.out.println(sayHelloToSomeone("Hannah"));
         System.out.println(sayHelloToSomeone("Victoria"));
         System.out.println(sayHelloToSomeone("William"));
         System.out.println(sayHelloToSomeone("Xavier"));
